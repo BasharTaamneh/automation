e9874e1efedb390785185a004267bb5c21240398
bashar adel taamneh <bashartaamneh55@gmail.com>
2021-11-04 02:06:28 +0200

    done coding automation

--- automation/automation.py
+++ automation/automation.py
@@ -1,41 +1,82 @@
 import re
 
-# with open("automation/potential-contacts.txt", "r") as f:
-#     email_content = f.read().split(" ")
 
-# emails = ""
-# for i in email_content:
-#     pattern = r'[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]'
-#     sequence = i
-#     if re.match(pattern, sequence):
-#         emails += "{} ".format(i) 
-#     else: pass
-# emails = sorted(emails.split(" "))
+def extract_contacts(file_path):
 
-# with open("automation/emails.txt", "w+") as f:
-#     for email in emails:
-#         f.write("{}\n".format(email))
+    phone_numbers = []
+    finall_phone_numbers = []
 
-#////////////////////////////////////////////////////////////////////////////////
+    with open(file_path, "r") as f:
+        email_content = f.read().split(" ")
 
-with open("automation/potential-contacts.txt", "r") as f:
-    phone_content = f.read().split(" ")
+    emails = ""
+    for i in email_content:
+        pattern = r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]"
+        sequence = i
+        if re.match(pattern, sequence):
+            emails += "{} ".format(i)
+        else:
+            pass
+    emails = sorted(emails.split(" "))
 
-# print(phone_content)
-phones = ""
-for i in phone_content:
-    pattern = r"^\D*(?:\d\D*){10,}$"
-    sequence = i
-    if re.search(pattern, sequence):
-        # print(i)
-        phones += "{} ".format(i) 
-    else: pass
-phones = sorted(phones.split(" "))
+    with open("automation/emails.txt", "w+") as f:
+        for email in emails:
+            f.write("{}\n".format(email))
 
-# for i in phones:
-#     if len(i.split(".")) == 2:
-#         first = i.split(".")[1]
-#     elif len(i.split(".")) == 1:
-#         first = i
-#     print(first)
-print(phones)
+    # ***************************************************************************************//
+
+    with open(file_path, "r") as f:
+        numbers = f.read()
+
+    match_num_1 = re.findall("\d{10}", numbers)
+    for phone_num in match_num_1:
+        phone_num = "206-" + phone_num[:3] + "-" + phone_num[3:6] + "-" + phone_num[6:]
+        phone_numbers.append(phone_num)
+
+    match_num_2 = re.findall("\(\d{3}\)\d{3}[-.]\d{4}", numbers)
+    for phone_num in match_num_2:
+        phone_num = "206-" + phone_num[1:4] + "-" + phone_num[5:8] + "-" + phone_num[9:]
+        phone_numbers.append(phone_num)
+
+    match_num_3 = re.findall("\d{3}[-.]\d{3}[-.]\d{4}", numbers)
+    for phone_num in match_num_3:
+        phone_num = "206-" + phone_num[:3] + "-" + phone_num[4:7] + "-" + phone_num[8:]
+        phone_numbers.append(phone_num)
+
+    phone_match_area_code_format_2 = re.findall(
+        "[+]\d{1}[-.]\d{3}[-.]\d{3}[-.]\d{4}", numbers
+    )
+    for phone_num in phone_match_area_code_format_2:
+        phone_num = "206-" + phone_num[3:]
+        phone_numbers.append(phone_num)
+
+    phone_match_area_code_format_1 = re.findall(
+        "\d{3}[-.]\d{3}[-.]\d{3}[-.]\d{4}", numbers
+    )
+    for phone_num in phone_match_area_code_format_1:
+        phone_num = phone_num
+        phone_numbers.append(phone_num)
+
+    # Removi Duplicates
+    phone_numbers = list(dict.fromkeys(phone_numbers))
+    # Sorting
+    phone_numbers.sort()
+
+    for i in phone_numbers:
+        finall_phone_numbers.append(i[4:16])
+
+    # print(len(findall_phone_numbers))
+    # print(findall_phone_numbers)
+    # print(phone_numbers)
+    # print(len(phone_numbers))
+    
+    with open("automation/phone_numbers.txt", "w+") as f:
+        for phone_num in finall_phone_numbers:
+            phone_num = phone_num + "\n"
+            f.write(phone_num)
+
+    finall_phone_numbers = []
+    return phone_numbers
+
+
+print(extract_contacts("automation/potential-contacts.txt"))
